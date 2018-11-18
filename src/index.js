0ddd84069d74f2b686095b61fdef351b5ecf418a
Alexander Ivankov <alexanderivankoff@gmail.com>
2018-11-19 00:04:15 +0300

    Proof of concept: migration on babel parser

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const acorn = require('acorn');
+const traverser = require('@babel/traverse');
+const types = require('@babel/types');
+const generate = require('@babel/generator');
+const parser = require('@babel/parser');
+
 const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
@@ -12,7 +16,7 @@ const port = process.env.PORT || 8080;
 app.get('/', (req, res) => {
   readFileAsync(path.join(__dirname, './css-in-js.js'), { encoding: 'utf8' })
     .then((data) => {
-      res.json(acorn.parse(data))
+      res.json(parser.parse(data))
     })
 })
 
